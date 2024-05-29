a950305150f88fff1066683f519e613568c93ef0
JoaoPauloFerrariSantAna <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2024-05-29 20:27:45 -0300

    Adding "graph" file

---
+++ graph.R
@@ -0,0 +1,20 @@
+install.packages("igraph")
+library(igraph)
+
+# vamos ler o arquivo "corpus" com "read.table"
+# <link da documentação>
+corpus <- read.table("C:/Users/Aula/Documents/r-code/corpus.txt", header = TRUE, sep = "\t")
+
+# transforme, agora, o conteúdo do arquivo "corpus" em um dataframe
+corpus <- as.data.frame(corpus)
+
+# tudo que for vazio, insira "NA"
+corpus[corpus == ''] <- NA
+
+# agora, vamos transformar "corpus" em uma matriz
+# vamos empilhar uma matriz com "corpus"
+matriz <- stack(corpus)
+
+# use a função "table" para achar a frequência da tabela
+# <doc>
+freq_matriz <- table(matriz)
